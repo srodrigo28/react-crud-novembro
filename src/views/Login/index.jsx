6ff636430f17a7050a3136f5d19bd1c647152ca5
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-11-02 13:34:22 -0300

    front finalizado

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react';
 import './styles.css';
+import axios from 'axios';
 
 export function Login(){
+    const url = "https://macatto-api2.vercel.app/login";
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
+    const [error, setError] = useState('')
+
+    const Validar = async (e) => {
+        e.preventDefault();
+        console.log(email, senha);
+
+        try{
+            const response = await axios.post(url,{
+                email: email,
+                senha: senha
+            })
+            console.log(response)
+        }catch(err){
+            if(error.response){
+                setError('1. Erro falha no servidor')
+            }else if(error.response == 400){
+                setError('2. Usuário ou senha inválidos')
+            }
+        }
+    }
 
     return(
         <>
-        <form autoComplete='off'>
+        <form autoComplete='on' className='cxLogin'>
             <h1 className="text-center mt-5 mb-3">Bem vindo ao Sistema</h1>
+            { error ? error.response : "" }
             <div className="row">
                 <div className="col">
                     <label htmlFor='email'>E-mail</label>
@@ -32,16 +55,12 @@ export function Login(){
                 </div>
             </div>
             <div className="row mt-2">
-                <div className="col">
-                    <button className="btn btn-outline-primary">Entrar</button>
+                <div className="col col-btn">
+                    <button className="btn btn-outline-primary" onClick={ (e) => Validar(e) }>Entrar</button>
                     <button className="btn btn-outline-success">Registrar</button>
                 </div>
             </div>
         </form>
-        <div>
-            <p>E-mail: { email } </p>
-            <p>Senha:  { senha } </p>
-        </div>
         </>
     )
 }
